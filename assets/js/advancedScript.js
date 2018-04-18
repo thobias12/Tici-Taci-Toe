03c0aed3fbc58f684ca7eb784e65fdb4a6090b98
Izæt <izet0004@elev.techcollege.dk>
2018-04-18 15:03:35 +0200

    PlaceMove

--- assets/js/advancedScript.js
+++ assets/js/advancedScript.js
@@ -1,13 +1,21 @@
 let playerId = 1;
 let winCombinations = [123, 459, 789, 147, 258, 369, 159, 357];
-let gameFrame = document.getElementsByClassName("gameFrame");
+let gameFrame = document.getElementById("gameFrame")
 let gameFields = gameFrame.querySelectorAll(".gameField");
 
-
 for (let i = 0; i < gameFields.length; i++) {
-    gameFields[i].onClick = placeMove(this);
+    gameFields[i].onclick = function () {
+        placeMove(this);
+    };
 }
 
 function placeMove(field) {
-    console.log(field);
+    if (playerId == 1) {
+        field.innerHTML = "O";
+        playerId++;
+    } else {
+        field.innerHTML = "X";
+        playerId--;
+    }
+    console.log(playerId);
 }
