384cfba4a61ade99c25bea4249c47bfcb19f6f53
thobias12 <thobiasslet@live.dk>
2018-04-19 12:31:29 +0200

    :poop:

--- assets/js/advancedScript.js
+++ assets/js/advancedScript.js
@@ -13,11 +13,11 @@ for (let i = 0; i < gameFields.length; i++) {
 
 function placeMove(field) {
     if (playerId == 1) {
-        field.innerHTML = '<svg viewBox="0 0 320 320" xml:space="preserve" id="circleSvg" xmlns="http://www.w3.org/2000/svg"><circle id="path" cx="160" cy="160" r="100" stroke="#69F0AE" fill="none" stroke-width="5" stroke-linecap="round"></circle></svg>';
+        field.innerHTML = '<svg viewBox="0 0 320 320" xml:space="preserve" id="circleSvg" xmlns="http://www.w3.org/2000/svg"><circle id="path" cx="160" cy="160" r="100" stroke="#69F0AE" fill="none" stroke-width="15" stroke-linecap="round"></circle></svg>';
         checkScore(playerId, player1, parseInt(field.id));
         playerId++;
     } else {
-        field.innerHTML = '<svg viewBox="0 0 60 60" xml:space="preserve" id="lineSvg" xmlns="http://www.w3.org/2000/svg"><line id="line" x1="0" y1="0" x2="60" y2="60" stroke="#69F0AE" stroke-width="2" /><line id="line2" x1="0" y1="60" x2="60" y2="0" stroke="#69F0AE" stroke-width="2" /></svg>';
+        field.innerHTML = '<svg viewBox="0 0 60 60" xml:space="preserve" id="lineSvg" xmlns="http://www.w3.org/2000/svg"><line id="line" x1="0" y1="0" x2="60" y2="60" stroke="#69F0AE" stroke-width="6" /><line id="line2" x1="0" y1="60" x2="60" y2="0" stroke="#69F0AE" stroke-width="6" /></svg>';
         checkScore(playerId, player2, parseInt(field.id));
         playerId--;
     }
