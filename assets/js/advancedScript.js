767bdf2359d72f4e2c18228df383307e7ea4c3dc
Izæt <izet0004@elev.techcollege.dk>
2018-04-19 12:39:48 +0200

    LeAlgoRiigThM

--- assets/js/advancedScript.js
+++ assets/js/advancedScript.js
@@ -1,6 +1,8 @@
 let playerId = 1;
 let player1 = [];
 let player2 = [];
+let svgCross = '<svg viewBox="0 0 60 60" xml:space="preserve" id="lineSvg" xmlns="http://www.w3.org/2000/svg"><line id="line" x1="0" y1="0" x2="60" y2="60" stroke="#69F0AE" stroke-width="2" /><line id="line2" x1="0" y1="60" x2="60" y2="0" stroke="#69F0AE" stroke-width="2" /></svg>';
+let svgCircle = '<svg viewBox="0 0 320 320" xml:space="preserve" id="circleSvg" xmlns="http://www.w3.org/2000/svg"><circle id="path" cx="160" cy="160" r="100" stroke="#69F0AE" fill="none" stroke-width="5" stroke-linecap="round"></circle></svg>';
 let winCombinations = ["123", "456", "789", "147", "258", "369", "159", "357"];
 let gameFrame = document.getElementById("gameFrame")
 let gameFields = gameFrame.querySelectorAll(".gameField");
@@ -12,18 +14,16 @@ for (let i = 0; i < gameFields.length; i++) {
 }
 
 function placeMove(field) {
-    if (playerId == 1) {
-        field.innerHTML = '<svg viewBox="0 0 320 320" xml:space="preserve" id="circleSvg" xmlns="http://www.w3.org/2000/svg"><circle id="path" cx="160" cy="160" r="100" stroke="#69F0AE" fill="none" stroke-width="5" stroke-linecap="round"></circle></svg>';
+    if (playerId == 1 && !field.innerHTML.includes("svg")) {
+        field.innerHTML = svgCircle;
         checkScore(playerId, player1, parseInt(field.id));
         playerId++;
-    } else {
-        field.innerHTML = '<svg viewBox="0 0 60 60" xml:space="preserve" id="lineSvg" xmlns="http://www.w3.org/2000/svg"><line id="line" x1="0" y1="0" x2="60" y2="60" stroke="#69F0AE" stroke-width="2" /><line id="line2" x1="0" y1="60" x2="60" y2="0" stroke="#69F0AE" stroke-width="2" /></svg>';
+    } else if (playerId == 2 && !field.innerHTML.includes("svg")) {
+        field.innerHTML = svgCross;
         checkScore(playerId, player2, parseInt(field.id));
         playerId--;
     }
-    // console.log(playerId);
 }
-
 
 function checkScore(playerId, playerScore, id) {
     playerScore.push(id);
@@ -37,8 +37,3 @@ function checkScore(playerId, playerScore, id) {
         }
     }
 }
-
-// player, id
-
-// player += id
-// if player sort contains console.log player
