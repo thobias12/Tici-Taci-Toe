fa7f7d302ad91799ec30d9535672defb417a4045
Izæt <izet0004@elev.techcollege.dk>
2018-04-19 10:23:27 +0200

    scriptie

--- assets/js/advancedScript.js
+++ assets/js/advancedScript.js
@@ -1,4 +1,6 @@
 let playerId = 1;
+let player1 = [];
+let player2 = [];
 let winCombinations = [123, 459, 789, 147, 258, 369, 159, 357];
 let gameFrame = document.getElementById("gameFrame")
 let gameFields = gameFrame.querySelectorAll(".gameField");
@@ -12,10 +14,28 @@ for (let i = 0; i < gameFields.length; i++) {
 function placeMove(field) {
     if (playerId == 1) {
         field.innerHTML = "O";
+        checkScore(playerId, player1, parseInt(field.id));
         playerId++;
     } else {
         field.innerHTML = "X";
+        checkScore(playerId, player2, parseInt(field.id));
         playerId--;
     }
-    console.log(playerId);
+    // console.log(playerId);
 }
+
+
+function checkScore(playerId, playerScore, id) {
+    playerScore.push(id);
+    playerScore = playerScore.sort(function (a, b) {
+        return a - b
+    }).join("");
+    if (playerScore.includes("456")) {
+        console.log("Player " + playerId + " Won!");
+    }
+}
+
+// player, id
+
+// player += id
+// if player sort contains console.log player
