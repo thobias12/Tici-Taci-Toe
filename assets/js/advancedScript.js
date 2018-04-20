b6ef26d4d89dbcf6ace35ec076c173ec669e6ab4
thobias12 <thobiasslet@live.dk>
2018-04-20 13:44:01 +0200

    :poop:

--- assets/js/advancedScript.js
+++ assets/js/advancedScript.js
@@ -10,53 +10,63 @@ let gameFrame = document.getElementById("gameFrame")
 let gameFields = gameFrame.querySelectorAll(".gameField");
 
 for (let i = 0; i < gameFields.length; i++) {
-    gameFields[i].onclick = function () {
-        placeMove(this);
-    };
+	gameFields[i].onclick = function () {
+		placeMove(this);
+	};
 }
 
 function placeMove(field) {
-    if (playerId == 1 && !field.innerHTML.includes("stroke")) {
-        field.innerHTML = svgCircle;
-        checkScore(playerId, player1, parseInt(field.id));
-        playerId++;
-    } else if (playerId == 2 && !field.innerHTML.includes("stroke")) {
-        field.innerHTML = svgCross;
-        checkScore(playerId, player2, parseInt(field.id));
-        playerId--;
-    }
+	if (playerId == 1 && !field.innerHTML.includes("stroke")) {
+		field.innerHTML = svgCircle;
+		checkScore(playerId, player1, parseInt(field.id));
+		playerId++;
+	} else if (playerId == 2 && !field.innerHTML.includes("stroke")) {
+		field.innerHTML = svgCross;
+		checkScore(playerId, player2, parseInt(field.id));
+		playerId--;
+	}
 }
 
 function checkScore(playerId, playerScore, id) {
-    moves++;
-    playerScore.push(id);
+	moves++;
+	playerScore.push(id);
 
-    for (let x = 0; x < winCombinations.length; x++) { // For every combination
-        let match = 0;
-        for (let i = 0; i < playerScore.length; i++) { // Check current playerScore against current combination index
-            if (winCombinations[x].toString().includes(playerScore[i])) { // Convert to string, compare if playerscore index exists in combination index
-                match++;
-                console.log(match);
-                if (match == 3) {
-                    if (playerId > 1) {
-                        document.getElementById("player2Score").innerHTML = +1;
-                        moves = 0;
-                    } else {
-                        document.getElementById("player1Score").innerHTML = +1;
-                        moves = 0;
-                    }
-                }
-            }
-        }
-    }
-    if (moves == 9) {
-        console.log("TIEIEIE");
-    }
+	for (let x = 0; x < winCombinations.length; x++) { // For every combination
+		let match = 0;
+		for (let i = 0; i < playerScore.length; i++) { // Check current playerScore against current combination index
+			if (winCombinations[x].toString().includes(playerScore[i])) { // Convert to string, compare if playerscore index exists in combination index
+				match++;
+				console.log(match);
+				if (match == 3) {
+					if (playerId > 1) {
+						document.getElementById("player2Score").innerHTML = +1;
+						moves = 0;
+						document.getElementById("winner").innerHTML = ("Player " + playerId + " Won");
+						setTimeout(clearBoard, 3000);
+					} else {
+						document.getElementById("player1Score").innerHTML = +1;
+						moves = 0;
+						document.getElementById("winner").innerHTML = ("Player " + playerId + " Won");
+						setTimeout(clearBoard, 3000);
+					}
+				}
+			}
+		}
+	}
+	if (moves == 9) {
+		document.getElementById("tiesScore").innerHTML = +1;
+		document.getElementById("winner").innerHTML = ("Game was tie");
+		setTimeout(clearBoard, 3000);
+	}
 }
 
 function clearBoard() {
-    var elements = document.getElementsByClassName("svgInput");
-    while (elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
-    }
+	var elements = document.getElementsByClassName("svgInput");
+	playerId = 1;
+	player1 = [];
+	player2 = [];
+	moves = 0;
+	while (elements.length > 0) {
+		elements[0].parentNode.removeChild(elements[0]);
+	}
 }
